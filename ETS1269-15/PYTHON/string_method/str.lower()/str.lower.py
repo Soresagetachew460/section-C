5f6fc94bbd3f3e6a741fd7db025ecbbf625ca4ba
Soresagetachew460 <Soresagetachew@gmail.com>
2025-03-22 15:32:40 +0300

    Update str.lower.py

--- ETS1269-15/PYTHON/string_method/str.lower()/str.lower.py
+++ ETS1269-15/PYTHON/string_method/str.lower()/str.lower.py
@@ -1,7 +1,5 @@
 # str_lower.py
 
 data = "PYTHON IS BEST PROGRAMMING LANGUAGE"
-lowered = data.lower()                  # => "python is best programming language"
-final = lowered[0].upper() + lowered[1:]  # Fix the first letter to uppercase
-print(final)                            # "Python is best programming language"
-
+result = data.lower()
+print(result)  # Expected output: "python is best programming language"
